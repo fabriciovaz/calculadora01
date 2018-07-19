b155d3b967ac58ce2287df37c2ef0d2c31b233f3
victorcaldas <contatos.caldas@gmail.com>
2018-07-19 15:25:52 -0300

    adicionado efeito da animacao de piscar no textcolor

--- app/src/main/java/com/example/fabricio/calculadora/MainActivity.java
+++ app/src/main/java/com/example/fabricio/calculadora/MainActivity.java
@@ -1,16 +1,16 @@
 package com.example.fabricio.calculadora;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
 import android.graphics.Color;
-import android.os.Handler;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
     private TextView tv1;
     private Button bt0;
     private Button bt1;
@@ -37,17 +37,202 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     double x;
     double y;
     double z;
-    int controle=0;
 
-
+    int controle = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeElements();
+    }
 
+    @Override
+    public void onClick(View v) {
 
+        switch (v.getId()) {
+            case R.id.bt0:
+                if (controle == 0) {
+                    tv1.setText("");
+                    controle = 1;
+                }
 
+                tv1.append("0");
+                x = Double.parseDouble(tv1.getText().toString());
+                break;
+
+            case R.id.bt1:
+                if (controle == 0) {
+                    tv1.setText("");
+                    controle = 1;
+                }
+                tv1.append("1"); //tv1 exibe o valor de x
+                x = Double.parseDouble(tv1.getText().toString()); //x recebe o proximo digito
+                break;
+
+            case R.id.bt2:
+                if (controle == 0) {
+                    tv1.setText("");
+                    controle = 1;
+                }
+                tv1.append("2");
+                x = Double.parseDouble(tv1.getText().toString());
+                break;
+
+            case R.id.bt3:
+                if (controle == 0) {
+                    tv1.setText("");
+                    controle = 1;
+                }
+                tv1.append("3");
+                x = Double.parseDouble(tv1.getText().toString());
+                break;
+
+            case R.id.bt4:
+                if (controle == 0) {
+                    tv1.setText("");
+                    controle = 1;
+                }
+                tv1.append("4");
+                x = Double.parseDouble(tv1.getText().toString());
+                break;
+
+            case R.id.bt5:
+                if (controle == 0) {
+                    tv1.setText("");
+                    controle = 1;
+                }
+                tv1.append("5");
+                x = Double.parseDouble(tv1.getText().toString());
+                break;
+
+            case R.id.bt6:
+                if (controle==0) {
+                    tv1.setText("");
+                    controle=1;
+                }
+                tv1.append("6");
+                x=Double.parseDouble(tv1.getText().toString());
+                break;
+
+            case R.id.bt7:
+                if (controle == 0) {
+                    tv1.setText("");
+                    controle=1;
+                }
+                tv1.append("7");
+                x = Double.parseDouble(tv1.getText().toString());
+                break;
+
+            case R.id.bt8:
+                if (controle == 0) {
+                    tv1.setText("");
+                    controle=1;
+                }
+                tv1.append("8");
+                x = Double.parseDouble(tv1.getText().toString());
+                break;
+
+            case R.id.bt9:
+                if (controle==0){tv1.setText("");controle=1;}
+                tv1.append("9");
+                x=Double.parseDouble(tv1.getText().toString());
+                break;
+
+            case R.id.btPonto:
+                if (controle==0){tv1.setText("");controle=1;}
+                tv1.append(".");
+                x=Double.parseDouble(tv1.getText().toString());
+                break;
+
+            case R.id.btC:
+                x=0;
+                controle=0;
+                tv1.setText("0");
+                break;
+
+            case R.id.btEnter:
+                w = z;
+                z = y;
+                y = x;
+                controle = 0;
+
+                // fisica fiz um codigo que vai te ajudar muito espero que goste <3
+                ObjectAnimator.ofObject(
+                        tv1, // objeto que vai ser animado
+                        "textColor", // propriedade que vai ser animada
+                        new ArgbEvaluator(), //  funcao de interpolacao
+                        Color.BLUE, // cor de inicio
+                        Color.RED // cor de fim
+                ).setDuration(1200) // duracao
+                        .start(); // manda ver!!!
+                break;
+
+            case R.id.btAf:
+                w = z;
+                z = y;
+                y = x;
+                controle = 0;
+                break;
+
+            case R.id.btSomar:
+                if (y + x - Math.round(y + x)==0) {
+                    tv1.setText(Double.toString(y+x).replace(".0",""));
+                }else{
+                    tv1.setText(Double.toString(y+x));
+                }
+
+                x=y+x;
+                y=z;
+                z=w;
+                w=0;
+                controle=0;
+                break;
+
+            case R.id.btSubtrair:
+                if (y-x - Math.round(y-x)==0) {
+                    tv1.setText(Double.toString(y-x).replace(".0",""));
+                }else{
+                    tv1.setText(Double.toString(y-x));
+                }
+
+                x=y-x;
+                y=z;
+                z=w;
+                w=0;
+                controle=0;
+                break;
+
+            case R.id.btMultiplicar:
+                if (y * x - Math.round(y * x) == 0) {
+                    tv1.setText(Double.toString(y*x).replace(".0",""));
+                }else{
+                    tv1.setText(Double.toString(y*x));
+                }
+
+                x = y * x;
+                y = z;
+                z = w;
+                w = 0;
+                controle = 0;
+                break;
+
+            case R.id.btDividir:
+                if (y/x - Math.round(y/x)==0) {
+                    tv1.setText(Double.toString(y / x).replace(".0", ""));
+                }else{
+                    tv1.setText(Double.toString(y / x));
+                }
+                x=y/x;
+                y=z;
+                z=w;
+                w=0;
+                controle=0;
+                break;
+        }
+    }
+
+    public void initializeElements(){
         tv1 = (TextView) findViewById(R.id.tV1);
         tv1.setText("0");
         bt0 = (Button) findViewById(R.id.bt0);
@@ -90,183 +275,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btA0.setOnClickListener(this);
         btT = (Button) findViewById(R.id.btT);
         btT.setOnClickListener(this);
-
-
     }
-
-
-
-    @Override
-    public void onClick(View v){
-
-        if (v.getId()==(R.id.bt0)){
-            if (controle==0){tv1.setText("");controle=1;}
-            tv1.append("0");
-            x=Double.parseDouble(tv1.getText().toString());
-
-            teste();
-
-        }
-
-
-        if (v.getId()==(R.id.bt1)){
-            if (controle==0){tv1.setText("");controle=1;}
-            tv1.append("1"); //tv1 exibe o valor de x
-            x=Double.parseDouble(tv1.getText().toString()); //x recebe o prximo digito
-
-        }
-
-        if (v.getId()==(R.id.bt2)){
-            if (controle==0){tv1.setText("");controle=1;}
-            tv1.append("2");
-            x=Double.parseDouble(tv1.getText().toString());
-
-        }
-
-        if (v.getId()==(R.id.bt3)){
-            if (controle==0){tv1.setText("");controle=1;}
-            tv1.append("3");
-            x=Double.parseDouble(tv1.getText().toString());
-
-        }
-        if (v.getId()==(R.id.bt4)){
-            if (controle==0){tv1.setText("");controle=1;}
-            tv1.append("4");
-            x=Double.parseDouble(tv1.getText().toString());
-
-        }
-        if (v.getId()==(R.id.bt5)){
-            if (controle==0){tv1.setText("");controle=1;}
-            tv1.append("5");
-            x=Double.parseDouble(tv1.getText().toString());
-
-        }
-        if (v.getId()==(R.id.bt6)){
-            if (controle==0){tv1.setText("");controle=1;}
-            tv1.append("6");
-            x=Double.parseDouble(tv1.getText().toString());
-
-        }
-        if (v.getId()==(R.id.bt7)){
-            if (controle==0){tv1.setText("");controle=1;}
-            tv1.append("7");
-            x=Double.parseDouble(tv1.getText().toString());
-
-        }
-        if (v.getId()==(R.id.bt8)){
-            if (controle==0){tv1.setText("");controle=1;}
-            tv1.append("8");
-            x=Double.parseDouble(tv1.getText().toString());
-
-        }
-        if (v.getId()==(R.id.bt9)){
-            if (controle==0){tv1.setText("");controle=1;}
-            tv1.append("9");
-            x=Double.parseDouble(tv1.getText().toString());
-
-        }
-
-        if (v.getId()==(R.id.btPonto)){
-            if (controle==0){tv1.setText("");controle=1;}
-            tv1.append(".");
-            x=Double.parseDouble(tv1.getText().toString());
-        }
-
-        if (v.getId()==(R.id.btC)){
-            x=0;
-            controle=0;
-            tv1.setText("0");
-        }
-
-        if (v.getId()==(R.id.btEnter)) {
-
-            w=z;
-            z=y;
-            y=x;
-            controle=0;
-
-        }
-
-
-        if (v.getId()==(R.id.btAf)) {
-
-            w=z;
-            z=y;
-            y=x;
-            controle=0;
-
-        }
-
-        if (v.getId()==(R.id.btSomar)){
-
-            if (y+x - Math.round(y+x)==0) {
-                tv1.setText(Double.toString(y+x).replace(".0",""));
-            }else{
-                tv1.setText(Double.toString(y+x));
-            }
-            x=y+x;
-            y=z;
-            z=w;
-            w=0;
-            controle=0;
-        }
-        if (v.getId()==(R.id.btSubtrair)){
-
-            if (y-x - Math.round(y-x)==0) {
-                tv1.setText(Double.toString(y-x).replace(".0",""));
-            }else{
-                tv1.setText(Double.toString(y-x));
-            }
-
-            x=y-x;
-            y=z;
-            z=w;
-            w=0;
-            controle=0;
-        }
-        if (v.getId()==(R.id.btMultiplicar)){
-
-            if (y*x - Math.round(y*x)==0) {
-                tv1.setText(Double.toString(y*x).replace(".0",""));
-            }else{
-                tv1.setText(Double.toString(y*x));
-            }
-
-            x=y*x;
-            y=z;
-            z=w;
-            w=0;
-            controle=0;
-        }
-        if (v.getId()==(R.id.btDividir)){
-            if (y/x - Math.round(y/x)==0) {
-                tv1.setText(Double.toString(y / x).replace(".0", ""));
-            }else{
-                tv1.setText(Double.toString(y / x));
-            }
-            x=y/x;
-            y=z;
-            z=w;
-            w=0;
-            controle=0;
-        }
-
-    }
-
-    public void teste() {
-
-
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-               // my_button.setBackgroundResource(R.drawable.defaultcard);
-            }
-        }, 2000);
-    }
-
-
-    }
-
-
-
+}
